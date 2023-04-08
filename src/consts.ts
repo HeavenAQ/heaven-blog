24d9e6bbde5c9db02bf1a388ad319405e20cc5e2
Heaven Chen <heavenchen@qnap.com>
2023-04-08 14:55:20 +0800

    Add theme toggle icon

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Heaven's Blog"
+export const SITE_TITLE = 'Heaven Chen'
 export const SITE_DESCRIPTION = 'A place to share my thoughts and ideas.'
